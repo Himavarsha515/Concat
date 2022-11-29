ca698d03c1b0caaa8cefd51e3dac6882bd3b8df6
Arlagadda Himavarsha <arlhimav@publicisgroupe.net>
2022-11-29 22:22:09 +0530

    added

---
+++ src/main/java/com/sapient/concateName.java
@@ -0,0 +1,13 @@
+package com.sapient;
+
+public class concateName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName="Arlagadda";
+		String lastName="Himavarsha";
+		System.out.println("Hello "+firstName+" "+lastName);
+
+	}
+
+}
